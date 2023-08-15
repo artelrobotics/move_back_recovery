5187e5ce70b94656599cea6d047ee5abf3aeb6ab
enco77 <tursunbaevsahboz@gmail.com>
2023-08-15 16:21:23 +0500

    first commit

---
+++ include/move_back_recovery/move_back_recovery.h
@@ -0,0 +1,55 @@
+#ifndef MOVE_BACK_RECOVERY_H_
+#define MOVE_BACK_RECOVERY_H_
+#include <nav_core/recovery_behavior.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <ros/ros.h>
+#include <costmap_2d/costmap_layer.h>
+#include <base_local_planner/costmap_model.h>
+#include <string>
+
+namespace move_back_recovery{
+  /**
+   * @class ClearCostmapRecovery
+   * @brief A recovery behavior that reverts the navigation stack's costmaps to the static map outside of a user-specified region.
+   */
+  class MoveBackClearRecovery : public nav_core::RecoveryBehavior {
+    public:
+      /**
+       * @brief  Constructor, make sure to call initialize in addition to actually initialize the object
+       * @param
+       * @return
+       */
+      MoveBackClearRecovery();
+
+      /**
+       * @brief  Initialization function for the ClearCostmapRecovery recovery behavior
+       * @param tf A pointer to a transform listener
+       * @param global_costmap A pointer to the global_costmap used by the navigation stack
+       * @param local_costmap A pointer to the local_costmap used by the navigation stack
+       */
+      void initialize(std::string name, tf2_ros::Buffer*,
+          costmap_2d::Costmap2DROS*, costmap_2d::Costmap2DROS* local_costmap);
+
+      /**
+       * @brief  Run the ClearCostmapRecovery recovery behavior. Reverts the
+       * costmap to the static map outside of a user-specified window and
+       * clears unknown space around the robot.
+       */
+      void runBehavior();
+
+     /**
+     * @brief  Destructor for the rotate recovery behavior
+     */
+     ~MoveBackClearRecovery();
+
+    private:
+      costmap_2d::Costmap2DROS* local_costmap_;
+      std::string name_;
+      ros::Publisher vel_pub;
+      bool initialized_;
+      base_local_planner::CostmapModel* world_model_;
+      double distance_backwards_;
+      double frequency_;
+  };
+};
+#endif
---
+++ src/move_back_recovery.cpp
@@ -0,0 +1,98 @@
+#include <move_back_recovery/move_back_recovery.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <boost/pointer_cast.hpp>
+#include <vector>
+#include <geometry_msgs/Twist.h>
+
+PLUGINLIB_EXPORT_CLASS(move_back_recovery::MoveBackClearRecovery, nav_core::RecoveryBehavior)
+
+namespace move_back_recovery
+{
+MoveBackClearRecovery::MoveBackClearRecovery(): local_costmap_(NULL), initialized_(false), world_model_(NULL)
+{
+}
+
+void MoveBackClearRecovery::initialize(std::string name, tf2_ros::Buffer*,
+    costmap_2d::Costmap2DROS*, costmap_2d::Costmap2DROS* local_costmap){
+  if(!initialized_){
+    name_ = name;
+    local_costmap_ = local_costmap;
+
+    //get some parameters from the parameter server
+    ros::NodeHandle private_nh("~/" + name_);
+    private_nh.param("distance_backwards", distance_backwards_, 0.1);
+    private_nh.param("frequency", frequency_, 20.0);
+    world_model_ = new base_local_planner::CostmapModel(*local_costmap_->getCostmap());
+    initialized_ = true;
+  }
+  else{
+    ROS_ERROR("You should not call initialize twice on this object, doing nothing");
+  }
+}
+MoveBackClearRecovery::~MoveBackClearRecovery()
+{
+    delete world_model_;
+}
+
+void MoveBackClearRecovery::runBehavior()
+{
+    if (!initialized_)
+    {
+        ROS_ERROR("This object must be initialized before runBehavior is called");
+        return;
+    }
+
+    if (local_costmap_ == NULL)
+    {
+        ROS_ERROR("The costmap passed to the MoveBackClearRecovery object cannot be NULL. Doing nothing.");
+        return;
+    }
+    ROS_WARN("MoveBackClearRecovery started.");
+    ros::Rate r(frequency_);
+    ros::NodeHandle nh;
+    vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+    geometry_msgs::PoseStamped global_pose;
+    std::vector<geometry_msgs::Point> oriented_footprint;
+    local_costmap_->getRobotPose(global_pose);
+    double current_x = global_pose.pose.position.x;
+    double current_y = global_pose.pose.position.y;
+    double start_x = current_x;
+    double start_y = current_y;
+    bool got_dist = false;
+
+    while (nh.ok() && !got_dist)
+        {
+            local_costmap_->getRobotPose(global_pose);
+            current_x = global_pose.pose.position.x;
+            current_y = global_pose.pose.position.y;
+            double dx = current_x - start_x;
+            double dy = current_y - start_y;
+            double moved_dist = std::sqrt(dx * dx + dy * dy);
+            if (moved_dist >= distance_backwards_)
+            {
+                got_dist = true;
+            }
+            else
+            {
+                local_costmap_->getOrientedFootprint(oriented_footprint);
+                double line_cost = world_model_->lineCost(oriented_footprint[0].x, oriented_footprint[0].y, oriented_footprint[1].x, oriented_footprint[1].y);
+                if (line_cost < 0)
+                {
+                    ROS_ERROR("MoveBackClearRecovery can't move back because there is a potential collision. Cost: %.2f", line_cost);
+                    return;
+                }
+
+                else
+                {
+                    ROS_WARN("MoveBackClearRecovery: line cost is %.2f", line_cost);
+                    geometry_msgs::Twist cmd_vel;
+                    cmd_vel.linear.x = -0.1;
+                    cmd_vel.angular.z = 0.0;
+                    vel_pub.publish(cmd_vel);
+                    r.sleep();
+                }
+
+            }
+        }
+}
+};  // namespace move_back_recovery
